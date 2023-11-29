a9f6dd9ffc51f671f469dd84534b7769f0d3cae6
Galin Iliev <galini@microsoft.com>
2023-11-28 23:28:54 -0800

    add flags for timeout and MaxTLS version

--- TlsChecker/tlsChecker.go
+++ TlsChecker/tlsChecker.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"time"
 
 	"flag"
 
@@ -18,11 +19,29 @@ func main() {
 		err  error
 	)
 
+	versions := map[uint16]string{
+		tls.VersionTLS10: "TLS 1.0",
+		tls.VersionTLS11: "TLS 1.1",
+		tls.VersionTLS12: "TLS 1.2",
+		tls.VersionTLS13: "TLS 1.3",
+	}
+
+	versionsFlag := map[string]uint16{
+		"TLS1.0": tls.VersionTLS10,
+		"TLS1.1": tls.VersionTLS11,
+		"TLS1.2": tls.VersionTLS12,
+		"TLS1.3": tls.VersionTLS13,
+	}
+
 	// https://alanfeng-test-vnetenc.azure-api.net/internal-status-0123456789abcdef
 	var targetUrl = flag.String("url", "https://google.com", "Provide URL to send requests to")
+	var maxTlsVersion = flag.String("maxTls", "TLS1.3", "Max TLS version to use")
+	var timeoutInSeconds = flag.Int("timeout", 10, "Timeout in seconds")
 	flag.Parse()
 
-	tlsConfig := http.DefaultTransport.(*http.Transport).TLSClientConfig
+	var tlsConfig *tls.Config = &tls.Config{
+		MaxVersion: versionsFlag[*maxTlsVersion],
+	}
 
 	client := &http.Client{
 		Transport: &http.Transport{
@@ -34,18 +53,12 @@ func main() {
 	}
 
 	restyClient := resty.NewWithClient(client)
+	restyClient.SetTimeout(time.Duration(*timeoutInSeconds) * time.Second)
 
 	resp, err := restyClient.R().Get(*targetUrl)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
-	}
-
-	versions := map[uint16]string{
-		tls.VersionTLS10: "TLS 1.0",
-		tls.VersionTLS11: "TLS 1.1",
-		tls.VersionTLS12: "TLS 1.2",
-		tls.VersionTLS13: "TLS 1.3",
 	}
 
 	fmt.Println("Calling GET:", *targetUrl)
